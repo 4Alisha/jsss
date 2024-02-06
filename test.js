91c2d68bfece199a4197fe0e3fa642e93a26f620
4Alisha <101681903+4Alisha@users.noreply.github.com>
2024-02-06 06:15:24 +0000

    not a number NaN

--- test.js
+++ test.js
@@ -6,3 +6,31 @@ let khushii
 console.table([name,university,rollNum,group,typeof(group),khushii])
 
 "use strict" //treat all js code as newer version
+
+//Data types
+/*
+
+1. number = range = 2^53
+2. Bigint 
+3. String
+4. Boolean
+5. Symbol
+6. Null
+7. Undefined
+
+
+
+*/
+
+console.table([typeof(Number),typeof(BigInt),typeof(String),typeof(Boolean),typeof(Symbol),typeof(null),typeof(undefined)])
+
+
+//type conversion
+let num=3456
+num=String(num)
+console.log(typeof num)
+
+let n='33aa44bb' 
+n=Number(n)
+console.log(typeof n)
+console.log(n)         // NaN not a number
